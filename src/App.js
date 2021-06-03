12474453a8f30834c15c0f901ee6daeb45451eec
Rokas <rookas.rudzenskas@gmail.com>
2021-06-03 14:32:40 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="app">
         <div className="app__body">
             <Sidebar />
+            <Chat />
         </div>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import "./styles/Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import IconButton from '@material-ui/core/IconButton';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Avatar from '@material-ui/core/Avatar';
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
+
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            This is cool
+            <div className="sidebar__header">
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+
+                </div>
+
+                <div className="sidebar__headerLeft">
+
+                </div>
+            </div>
         </div>
     );
 };
