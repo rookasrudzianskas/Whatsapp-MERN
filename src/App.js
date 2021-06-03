4eca881250e59e872460a1d5fddb7669d43f5148
Rokas <rookas.rudzenskas@gmail.com>
2021-06-03 13:16:42 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h1>Whatsapp 🚀</h1>
+      <Sidebar />
     </div>
   );
 }
