bf48d3bda2511dfb2d8be2a802a49ff0209cc126
Rokas <rookas.rudzenskas@gmail.com>
2021-06-04 09:28:54 +0300

    Roaks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import {useEffect} from "react";
 
 function App() {
+
+    useEffect(() => {
+
+        const pusher = new Pusher('be01aa18ea94a4ff1264', {
+            cluster: 'eu'
+        });
+
+        const channel = pusher.subscribe('messages');
+        channel.bind('inserted', (data) => {
+            alert(JSON.stringify(data));
+        });
+    }, []);
+
   return (
     <div className="app">
         <div className="app__body">
